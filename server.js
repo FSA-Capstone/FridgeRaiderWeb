53935c21154c33cd6b43153a43a8e5a011746372
Nishtha Malhotra <nishthamalhotra@gmail.com>
2018-11-18 14:26:46 -0500

    Setup

---
+++ server.js
@@ -0,0 +1,14 @@
+const path = require('path');
+const express = require('express');
+const app = express();
+const PORT = process.env.PORT || 4000;
+
+app.use("/dist", express.static(path.join(__dirname, "dist")));
+
+// main route
+app.get('/', (req, res, next) => {
+    res.sendFile(path.join(__dirname, "index.html"));
+})
+
+app.listen(PORT, () => console.log(`Listening on Port ${PORT}`));
+
