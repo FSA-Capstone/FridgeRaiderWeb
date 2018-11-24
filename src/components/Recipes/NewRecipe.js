3680138d2713367d84829129b4ca7094213eb7bf
David Acconcio <dacconcio@gmail.com>
2018-11-24 14:40:59 -0500

    increased size of tabs

--- src/components/Recipes/NewRecipe.js
+++ src/components/Recipes/NewRecipe.js
@@ -1,14 +1,17 @@
-import React, { Fragment } from 'react'
-import { Typography } from '@material-ui/core'
+import React, { Fragment } from 'react';
+import { Typography } from '@material-ui/core';
 
 const NewRecipe = () => {
   return (
     <Fragment>
-      <Typography variant="title" style={{marginTop: "10vh", marginBottom: "5vh", fontWeight: "bold"}}>
+      <Typography
+        variant="title"
+        style={{ marginTop: '10vh', marginBottom: '5vh', fontWeight: 'bold' }}
+      >
         New Recipe...
       </Typography>
     </Fragment>
-  )
-}
+  );
+};
 
-export default NewRecipe
+export default NewRecipe;
--- src/components/User/MyRecipes.js
+++ src/components/User/MyRecipes.js
@@ -1,38 +1,43 @@
-import React, { Component, Fragment } from 'react'
-import { Tabs, Tab } from '@material-ui/core'
+import React, { Component, Fragment } from 'react';
+import { Tabs, Tab } from '@material-ui/core';
 import MyUploadedRecipes from './MyUploadedRecipes';
 import MySavedRecipes from './MySavedRecipes';
 import NewRecipe from '../Recipes/NewRecipe';
 
- class MyRecipes extends Component {
-    
+class MyRecipes extends Component {
   constructor() {
-    super()
+    super();
     this.state = {
-        value: 0
-    }
-    this.handleChange = this.handleChange.bind(this)     
+      value: 0
+    };
+    this.handleChange = this.handleChange.bind(this);
   }
 
   handleChange(event, value) {
-    this.setState({ value })
+    this.setState({ value });
   }
 
   render() {
-    const { value } = this.state
-    const { handleChange } = this
-      return (
-        <Fragment>
-            <Tabs value={value} onChange={handleChange} centered >
-                <Tab label="My Uploaded Recipes" />
-                <Tab label="My Saved Recipes" />
-                <Tab label="New Recipe" />
-            </Tabs>
-            { value === 0 && <MyUploadedRecipes />}
-            { value === 1 && <MySavedRecipes />}
-            { value === 2 && <NewRecipe />}
-        </Fragment>
-    )
+    const { value } = this.state;
+    const { handleChange } = this;
+    return (
+      <Fragment>
+        <Tabs value={value} onChange={handleChange} centered>
+          <Tab
+            label={<span style={{ fontSize: 20 }}>My Uploaded Recipies</span>}
+          />
+          <Tab
+            label={<span style={{ fontSize: 20 }}>My Saved Recipies</span>}
+          />
+          <Tab label={<span style={{ fontSize: 20 }}>New Recipie</span>} />
+        </Tabs>
+        <div id="main">
+          {value === 0 && <MyUploadedRecipes />}
+          {value === 1 && <MySavedRecipes />}
+          {value === 2 && <NewRecipe />}
+        </div>
+      </Fragment>
+    );
   }
 }
- export default MyRecipes 
+export default MyRecipes;
