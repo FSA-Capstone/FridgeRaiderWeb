8cd789f035f6cbf3096b768a844d150b2d7fb01f
David Acconcio <dacconcio@gmail.com>
2018-11-24 12:49:09 -0500

    refactor

--- src/reducers/recipe.js
+++ src/reducers/recipe.js
@@ -16,7 +16,7 @@ const getRecipesForIngredients = (ingredients) => {
   return (dispatch) => {
 
     //TO DO: This needs to be changed and ingredients need to be passed (once DB is set)
-    return axios.get(`${process.env.API_URL}/api/recipes`)
+    return axios.get(`http://localhost:3000/api/recipes`)
       .then(res => res.data)
       .then(recipes => dispatch(_getRecipes(recipes)))
       .catch(error => console.log(error))
