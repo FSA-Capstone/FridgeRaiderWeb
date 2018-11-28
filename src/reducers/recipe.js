e1461171d4cffa3d03a04610c92fc8d2ee008e53
David Acconcio <dacconcio@gmail.com>
2018-11-27 21:32:57 -0500

    fix

--- src/reducers/recipe.js
+++ src/reducers/recipe.js
@@ -3,6 +3,7 @@ import axios from 'axios';
 // action constants
 const GET_RECIPES = 'GET_RECIPES';
 const ADD_RECIPE = 'ADD_RECIPE';
+const GET_RECIPE = 'GET_RECIPE';
 
 const _addRecipe = recipe => {
   return {
@@ -11,7 +12,6 @@ const _addRecipe = recipe => {
   };
 };
 
-const GET_RECIPE = 'GET_RECIPE';
 
 // action creators
 const _getRecipes = recipes => {
@@ -25,55 +25,62 @@ const _getRecipe = recipe => {
   return {
     type: GET_RECIPE,
     recipe
-  }
-}
+  };
+};
 
 // thunks
 
 const createNewRecipe = recipe => {
   return dispatch => {
-    console.log(recipe)
+    console.log(recipe);
     return axios
-      .post('http://localhost:3000/api/recipes/', recipe)
-      .then(response => dispatch(_addRecipe(response.data)))
+      .post(`${process.env.API_URL}/api/recipes/`, recipe)
+      .then(response => dispatch(_addRecipe(response.data)));
   };
 };
 
 const getRecipesForIngredients = ingredients => {
   return dispatch => {
     //TO DO: This needs to be changed and ingredients need to be passed (once DB is set)
+    return (
+      axios
+        .get(
+          `${process.env.API_URL}/api/recipes?ingredients=${encodeURI(
+            ingredients
+          )}`
+        )
+        // return axios.get(`${process.env.API_URL}/api/recipes?ingredients=${encodeURI(ingredients.join(','))}`)
+        .then(res => res.data)
+        .then(recipes => dispatch(_getRecipes(recipes)))
+        .catch(error => console.log(error))
+    );
+  };
+};
 
-    return axios.get(`${process.env.API_URL}/api/recipes?ingredients=${encodeURI(ingredients)}`)
-    // return axios.get(`${process.env.API_URL}/api/recipes?ingredients=${encodeURI(ingredients.join(','))}`)
+const getRecipe = id => {
+  return dispatch => {
+    //TO DO: This needs to be changed and ingredients need to be passed (once DB is set)
+    return axios
+      .get(`${process.env.API_URL}/api/recipes/${id}`)
       .then(res => res.data)
-      .then(recipes => dispatch(_getRecipes(recipes)))
+      .then(recipe => dispatch(_getRecipe(recipe)))
       .catch(error => console.log(error));
   };
 };
 
-
-const getRecipe = (id) => {
-  return (dispatch) => {
-    //TO DO: This needs to be changed and ingredients need to be passed (once DB is set)
-    return axios.get(`${process.env.API_URL}/api/recipes/${id}`)
-      .then(res => res.data)
-      .then(recipe => dispatch(_getRecipe(recipe)))
-      .catch(error => console.log(error))
-  };
-}
-
-
 // reducer
 const recipeReducer = (state = [], action) => {
   switch (action.type) {
+    case ADD_RECIPE:
+      return [...state, action.recipe];
     case GET_RECIPES:
       state = action.recipes;
       break;
 
     case GET_RECIPE:
-      const newState = state.filter( recipe => recipe.id !== action.recipe.id)
-      newState.push(action.recipe)
-      state = newState
+      const newState = state.filter(recipe => recipe.id !== action.recipe.id);
+      newState.push(action.recipe);
+      state = newState;
       break;
   }
   return state;
