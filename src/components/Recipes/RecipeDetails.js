5e4752b3093cbad106fcb92ded45a4859295bc73
Nicholas Melhado <n.melhado@gmail.com>
2018-11-29 20:27:59 -0500

    reviews

--- src/components/Recipes/RecipeDetails.js
+++ src/components/Recipes/RecipeDetails.js
@@ -1,15 +1,21 @@
 import React, { Component, Fragment } from 'react';
 import PropTypes from 'prop-types';
-import { withStyles, Card, CardHeader, CardContent, Typography, Divider } from '@material-ui/core';
+import { withStyles, Card, CardHeader, CardContent, Typography, Divider, TextField, MenuItem, MySnackbarContentWrapper, Button } from '@material-ui/core';
 import { connect } from 'react-redux';
-import { getRecipe } from '../../store';
+import { getRecipe, postReview } from '../../store';
 
 class RecipeDetails extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      recipe: {}
+      recipe: {},
+      rating: '',
+      description: '',
+      warningRating: false,
+      warningDescription: false
     }
+    this.handleChange = this.handleChange.bind(this)
+    this.postReview = this.postReview.bind(this)
   }
 
   componentDidMount() {
@@ -24,9 +30,51 @@ class RecipeDetails extends Component {
     }
   }
 
+  handleChange(e) {
+    const state = this.state
+    state[e.target.name] = e.target.value * 1 ? e.target.value * 1 : e.target.value
+    this.setState({
+      state 
+    })
+  }
+
+  postReview(e) {
+    e.preventDefault()
+    let valid = true
+    if(isNaN(this.state.rating) || this.state.rating === null) {
+      valid = false;
+      this.setState({
+        warningRating: true
+      })
+    } else if(this.state.warningRating){
+      this.setState({
+        warningRating: false
+      })
+    }
+    if(isNaN(this.state.description) || this.state.description === null) {
+      valid = false;
+      this.setState({
+        warningDescription: true
+      })
+    } else if(this.state.warningDescription){
+      this.setState({
+        warningDescription: false
+      })
+    }
+    const review = {
+      rating: this.state.rating,
+      description: this.state.description
+    }
+    if(valid) {
+      this.props.postReview( this.state.recipe.id, this.props.authenticatedUser.id, this.props.history )
+    }
+  }
+
   render() {
     const { classes } = this.props;
-    const { recipe } = this.state
+    const { recipe, rating, description, warningDescription, warningRating } = this.state
+    const { handleChange, postReview } = this
+    const ratings = [1, 2, 3, 4, 5]
     console.log(recipe)
     if(!recipe.id) return null;
 
@@ -66,10 +114,79 @@ class RecipeDetails extends Component {
               {recipe.instructions}
             </div>
           </div>
-          <div className={classes.container}>
-            <div style={{ gridColumnEnd: 'span 12' }}>
-              Reviews....
-            </div>
+        </div>
+        <div className={classes.container}>
+          <div style={{ gridColumnEnd: 'span 12' }}>
+            <h2 className="reviewHead">Reviews</h2>
+              {this.props.authenticatedUser.id ?
+              <form onSubmit={() => postReview()}>
+                <TextField
+                  id="rating"
+                  name="rating"
+                  select
+                  label="Select"
+                  className="textField"
+                  value={rating}
+                  onChange={handleChange}
+                  SelectProps={{
+                    MenuProps: {
+                      className: classes.menu,
+                    },
+                  }}
+                  helperText="Please rate the recipe."
+                  margin="normal"
+                  variant="outlined"
+                >
+                  {ratings.map(rating => (
+                    <MenuItem key={rating} value={rating}>
+                      {`${rating} Star${rating > 1 ? 's' : ''}`}
+                    </MenuItem>
+                  ))}
+                </TextField>
+                {
+                  warningRating ? 
+                    <MySnackbarContentWrapper
+                      variant="error"
+                      className={margin}
+                      value={description}
+                      message="Please rate the recipe!"
+                    /> : 
+                    null
+                }
+                <TextField
+                  id="description"
+                  name="description"
+                  label="Multiline Placeholder"
+                  placeholder="Placeholder"
+                  onChange={handleChange}
+                  multiline
+                  className="textFieldLarge"
+                  margin="normal"
+                  variant="outlined"
+                />
+                {
+                  warningDescription ? 
+                    <MySnackbarContentWrapper
+                      variant="error"
+                      className={classes.margin}
+                      message="Please write a review!"
+                    /> : 
+                    null
+                }
+                <Button variant="contained" aria-label="Submit">
+                  Submit
+                </Button>
+              </form>
+              :
+              null}
+              {
+                recipe.reviews.map((review, index) => 
+                  <div key={index} className="review">
+                    <h3 className="reviewAuthor">{review.properties.name}</h3>
+                    <img src={`/dist/rating/${review.relation.rating}.png`} className="rating" />
+                    <p className="reviewBody">{review.relation.description}</p>
+                  </div> )
+              }
           </div>
         </div>
       </Fragment>
@@ -86,20 +203,32 @@ const styles = theme => ({
   },
   divider: {
     margin: `${theme.spacing.unit * 2}px 0`,
-  }
+  },
+  textField: {
+    marginLeft: theme.spacing.unit,
+    marginRight: theme.spacing.unit,
+  },
+  textFieldLarge: {
+    width: "1000px",
+    maxWidth: "80%",
+    marginLeft: theme.spacing.unit,
+    marginRight: theme.spacing.unit,
+  },
 });
 
-const mapStateToProps = ({recipes},{ id }) => {
+const mapStateToProps = ({recipes, authenticatedUser},{ id }) => {
   const recipe = recipes.filter( recipe => recipe.id === id).pop()
   return {
     recipe,
+    authenticatedUser,
     id
   };
 };
 
 const mapDispatchToProps = (dispatch) => {
   return {
-    getRecipe: (id) => dispatch(getRecipe(id))
+    getRecipe: (id) => dispatch(getRecipe(id)),
+    postReview: (recipeId, userId, review) => dispatch(postReview(id))
   }
 }
 
--- src/reducers/recipe.js
+++ src/reducers/recipe.js
@@ -4,79 +4,85 @@ import axios from 'axios';
 const GET_RECIPES = 'GET_RECIPES';
 const ADD_RECIPE = 'ADD_RECIPE';
 
-const _addRecipe = recipe => {
-  return {
-    type: ADD_RECIPE,
-    recipe
-  };
+const _addRecipe = (recipe) => {
+	return {
+		type: ADD_RECIPE,
+		recipe
+	};
 };
 
 const GET_RECIPE = 'GET_RECIPE';
 
 // action creators
-const _getRecipes = recipes => {
-  return {
-    type: GET_RECIPES,
-    recipes
-  };
+const _getRecipes = (recipes) => {
+	return {
+		type: GET_RECIPES,
+		recipes
+	};
 };
 
-const _getRecipe = recipe => {
-  return {
-    type: GET_RECIPE,
-    recipe
-  }
-}
+const _getRecipe = (recipe) => {
+	return {
+		type: GET_RECIPE,
+		recipe
+	};
+};
 
 // thunks
 
-const createNewRecipe = recipe => {
-  return dispatch => {
-    console.log(recipe)
-    return axios
-      .post('http://localhost:3000/api/recipes/', recipe)
-      .then(response => dispatch(_addRecipe(response.data)))
-  };
+const createNewRecipe = (recipe) => {
+	return (dispatch) => {
+		console.log(recipe);
+		return axios
+			.post(`${process.env.API_URL}/api/recipes/`, recipe)
+			.then((response) => dispatch(_addRecipe(response.data)));
+	};
 };
 
-const getRecipesForIngredients = ingredients => {
-  return dispatch => {
-    //TO DO: This needs to be changed and ingredients need to be passed (once DB is set)
-
-    return axios.get(`${process.env.API_URL}/api/recipes?ingredients=${encodeURI(ingredients)}`)
-    // return axios.get(`${process.env.API_URL}/api/recipes?ingredients=${encodeURI(ingredients.join(','))}`)
-      .then(res => res.data)
-      .then(recipes => dispatch(_getRecipes(recipes)))
-      .catch(error => console.log(error));
-  };
+const getRecipesForIngredients = (ingredients) => {
+	return (dispatch) => {
+		return axios
+			.get(`${process.env.API_URL}/api/recipes?ingredients=${encodeURI(ingredients)}`)
+			.then((res) => res.data)
+			.then((recipes) => dispatch(_getRecipes(recipes)))
+			.catch((error) => console.log(error));
+	};
 };
-
 
 const getRecipe = (id) => {
-  return (dispatch) => {
-    //TO DO: This needs to be changed and ingredients need to be passed (once DB is set)
-    return axios.get(`${process.env.API_URL}/api/recipes/${id}`)
-      .then(res => res.data)
-      .then(recipe => dispatch(_getRecipe(recipe)))
-      .catch(error => console.log(error))
-  };
-}
+	return (dispatch) => {
+		return axios
+			.get(`${process.env.API_URL}/api/recipes/${id}`)
+			.then((res) => res.data)
+			.then((review) => dispatch(_getRecipe(review)))
+			.catch((error) => console.log(error));
+	};
+};
 
+const postReview = (recipeId, userId, review) => {
+	return (dispatch) => {
+		return axios
+			.put(`${process.env.API_URL}/api/recipes/${recipeId}/review/${userId}`, review)
+			.then((res) => res.data)
+			.then((recipe) => dispatch(_getRecipe(recipe)))
+			.catch((error) => console.log(error));
+	};
+};
 
 // reducer
 const recipeReducer = (state = [], action) => {
-  switch (action.type) {
-    case GET_RECIPES:
-      state = action.recipes;
-      break;
+	switch (action.type) {
+		case GET_RECIPES:
+			state = action.recipes;
+			break;
 
-    case GET_RECIPE:
-      const newState = state.filter( recipe => recipe.id !== action.recipe.id)
-      newState.push(action.recipe)
-      state = newState
-      break;
-  }
-  return state;
+		case GET_RECIPE:
+			const newState = state.filter((recipe) => recipe.id !== action.recipe.id);
+			newState.push(action.recipe);
+			state = newState;
+			break;
+	}
+	return state;
 };
 
-export { createNewRecipe, recipeReducer, getRecipesForIngredients, getRecipe };
+export { createNewRecipe, recipeReducer, getRecipesForIngredients, getRecipe, postReview };
