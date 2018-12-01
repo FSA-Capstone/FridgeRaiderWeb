b5feef510a5c02bcff28b456bda406d4f1df9275
nmelhado <39418000+nmelhado@users.noreply.github.com>
2018-12-01 15:35:37 -0500

    Update store.js

--- src/store.js
+++ src/store.js
@@ -1,9 +1,23 @@
 import { createStore, applyMiddleware, combineReducers } from 'redux';
 import logger from 'redux-logger';
 import thunk from 'redux-thunk';
-import { recipeReducer, createNewRecipe, getRecipesForIngredients, getRecipe, postReview } from './reducers/recipe';
+import {
+  recipeReducer,
+  createNewRecipe,
+  getRecipesForIngredients,
+  getRecipe,
+  postReview
+} from './reducers/recipe';
+import {
+  login,
+  checkForLoggedInGoogleUser,
+  logout,
+  authenticatedUserReducer,
+  setAuthenticatedUserOnRefresh,
+  exchangeTokenForAuth,
+  registerNewUser
+} from './reducers/authenticatedUser';
 import { ingredientReducer, getIngredients, setIngredients, addIngredient, removeIngredient } from './reducers/ingredients';
-import { login, checkForLoggedInGoogleUser, logout, authenticatedUserReducer, exchangeTokenForAuth, registerNewUser } from './reducers/authenticatedUser';
 import { getCategories, categoryReducer } from './reducers/category';
 import { cuisineReducer } from './reducers/cuisine';
 
