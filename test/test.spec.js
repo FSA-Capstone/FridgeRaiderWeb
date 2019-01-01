ca4cc5551c02a235c1ce4bc4172f73c10fb38106
David Acconcio <dacconcio@gmail.com>
2019-01-01 17:17:05 -0500

    package.json

--- test/test.spec.js
+++ test/test.spec.js
@@ -1,9 +1,17 @@
 const expect = require('chai').expect;
+import Enzyme from 'enzyme';
+import EnzymeAdapter from 'enzyme-adapter-react-16';
+Enzyme.configure({ adapter: new EnzymeAdapter() });
 
 describe('Travis Test', () => {
+  it('Test 1', () => {
+    expect(1 + 1).to.equal(2);
+  });
+});
 
-    it('Test 1', () => {
-        expect( 1 + 1 ).to.equal(2);
-    });
-
+describe('Recipe Pages', () => {
+  beforeEach('set up wrapper', () => {
+    wrapper = shallow(<Recipies />);
+  });
+  it('correctly filters by recipe', () => {});
 });
